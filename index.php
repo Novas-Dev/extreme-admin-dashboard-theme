1edbd70bcb8755472fe273fedc48510cbd00a694
mypablo4@gmail.com <mypablo4@gmail.com>
2021-07-29 16:31:02 +0300

    extreme blue admin dashboard theme

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ Version: 1.0
 add_action('admin_head', 'my_custom_fonts');
 
 function my_custom_fonts() {
-  echo "<link rel='stylesheet' href='../wp-content/plugins/admin-dashboard/style.css'>";
+  echo "<link rel='stylesheet' href='../wp-content/plugins/admin-dashboard/assets/css/dash-style.css'>";
   echo  '<link rel="preconnect" href="https://fonts.googleapis.com">';
   echo  '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
   echo  '<link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">';

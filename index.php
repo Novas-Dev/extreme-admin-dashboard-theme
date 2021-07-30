403046c50772e1858c729442dd7d9ba8a78fd1f4
mypablo4@gmail.com <mypablo4@gmail.com>
2021-07-30 12:44:55 +0300

    release

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 /*
-Plugin Name: Admin Dashboard
+Plugin Name: Extreme Admin Dashboard Theme
 Plugin URL:
-Description: Customize Admin Dashboard
+Description: One click Plugin - Just enable and enjoy your custom admin and login page
 Author: Novas Dev. Team
-Author URL:
 Version: 1.0
 */
 
@@ -13,14 +12,14 @@ Version: 1.0
 add_action('admin_head', 'my_custom_fonts');
 
 function my_custom_fonts() {
-  echo "<link rel='stylesheet' href='../wp-content/plugins/admin-dashboard/assets/css/dash-style.css'>";
+  echo "<link rel='stylesheet' href='../wp-content/plugins/extreme-admin-dashboard-theme/assets/css/dash-style.css'>";
   echo  '<link rel="preconnect" href="https://fonts.googleapis.com">';
   echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
   echo  '<link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">';
 }
 
 function extreme_login_css() {
-    echo "<link rel='stylesheet' href='wp-content/plugins/admin-dashboard/assets/css/login-style.css'>";
+    echo "<link rel='stylesheet' href='wp-content/plugins/extreme-admin-dashboard-theme/assets/css/login-style.css'>";
     echo  '<link rel="preconnect" href="https://fonts.googleapis.com">';
     echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>';
     echo  '<link href="https://fonts.googleapis.com/css2?family=Krona+One&display=swap" rel="stylesheet">';
